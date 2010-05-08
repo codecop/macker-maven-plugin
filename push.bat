f5005422d897a784a45ce5452ee03cd71f33b0e3
Peter Kofler <peter.kofler@code-cop.org>
2010-05-09 00:23:58 +0200

    create patch folder

--- push.bat
+++ push.bat
@@ -1,3 +1,3 @@
-@rem call hg push https://macker-maven-plugin.code-cop-code.googlecode.com/hg/
-@call hg push
+@rem call hg push https://macker-maven-plugin.code-cop-code.googlecode.com/hg/ %*
+@call hg push %*
 @pause
