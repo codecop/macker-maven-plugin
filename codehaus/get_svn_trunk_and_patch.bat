8342735e1b92f42974a49ed904d0ef5ba267c585
Peter Kofler <peter.kofler@code-cop.org>
2010-05-11 22:43:55 +0200

    second patch, auto patch script

--- codehaus/get_svn_trunk_and_patch.bat
+++ codehaus/get_svn_trunk_and_patch.bat
@@ -1,12 +1,39 @@
-rmdir /S /Q trunk-svn-patch
+setlocal
+set S=macker-maven-plugin\codehaus
+set P=macker-maven-plugin_patch
+set MAVEN_OPTS=-Xmx512m
+
+cd ..\..
+
+rmdir /S /Q %P%
 
 rem get current trunk
-call svn co http://svn.codehaus.org/mojo/trunk/sandbox/macker-maven-plugin/ trunk-svn-patch
+call svn co http://svn.codehaus.org/mojo/trunk/sandbox/macker-maven-plugin/ %P%
 
 rem apply patch
-cd trunk-svn-patch
-copy ..\..\pom.xml
-xcopy ..\..\src .\src /V /S /Y
+cd %P%
+copy ..\macker-maven-plugin\pom.xml
+xcopy ..\macker-maven-plugin\src .\src /V /S /Y
+rem copy ..\%S%\patch.project .project
 cd ..
 
+rem test it
+cd %P%
+call mvn clean install site
+cd ..
+pause
+
+rem clean in again and create patch
+cd %P%
+rem del .project
+call mvn clean
+cd src
+call svn add test
+cd ..
+call svn diff > ..\%S%\ExtendedConfAndTestCases.diff
+cd ..
+
+cd %S%
+
+endlocal
 pause
