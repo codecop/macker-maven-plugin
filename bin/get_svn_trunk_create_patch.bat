1a4da2f92db935754baf194eec4bcc6afd7f366e
Peter Kofler <peter.kofler@code-cop.org>
2010-08-07 14:15:02 +0200

    fix patch script

--- bin/get_svn_trunk_create_patch.bat
+++ bin/get_svn_trunk_create_patch.bat
@@ -14,19 +14,24 @@ rem apply patch
 cd %P%
 copy ..\macker-maven-plugin\pom.xml
 xcopy ..\macker-maven-plugin\src .\src /V /S /Y
-del .\src\org\codehaus\mojo\macker\LinkedMacker.java
+del .\src\main\java\org\codehaus\mojo\macker\LinkedMacker.java
 cd ..
 
 rem test it
 cd %P%
-call mvn clean install site
+call mvn clean verify site
 cd ..
 pause
 
 rem clean in again and create patch
 cd %P%
 call mvn clean
-call svn add 
+call svn add src\main\java\org\codehaus\mojo\macker\*.java
+call svn add src\site\apt\examples
+call svn add src\test
+call svn st
+pause
+
 call svn diff > ..\%S%\ExtendedConfAndForked.diff
 cd ..
 
